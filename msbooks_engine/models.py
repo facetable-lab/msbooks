5e2d2ad50398b7e8fd0a65d66ec84104cba64b08
facetable <F201283070480f>
2022-11-18 23:31:18 +0300

    Русификация модели

--- msbooks_engine/models.py
+++ msbooks_engine/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class Subscriber(models.Model):
-    email = models.EmailField()
-    name = models.CharField(max_length=128)
+    email = models.EmailField(verbose_name='Почта')
+    name = models.CharField(max_length=128, verbose_name='Имя')
 
     def __str__(self):
         return f'{self.name} - {self.email}'
+
+    class Meta:
+        verbose_name = 'подписчик'
+        verbose_name_plural = 'подписчики'
