b04b6730e2ad4e14e3744c1f65e649a50b9af7a7
facetable <F201283070480f>
2022-11-18 23:11:17 +0300

    Модель подписчика

--- msbooks_engine/models.py
+++ msbooks_engine/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Subscriber(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f'{self.name} - {self.email}'
