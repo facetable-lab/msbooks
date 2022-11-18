d55045775584a7c250223fee9fbf8dae82044fdc
facetable <F201283070480f>
2022-11-18 23:10:42 +0300

    Регистрация и отображение модели подписчика

--- msbooks_engine/admin.py
+++ msbooks_engine/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+
+from .models import Subscriber
+
+
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', '__str__')
+    ordering = ('email',)
+
+
+admin.site.register(Subscriber, SubscriberAdmin)
