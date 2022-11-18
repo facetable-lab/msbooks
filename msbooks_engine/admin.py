7bfdd2ac0adfb188ae3ed507b38a75fe0c5f0888
facetable <F201283070480f>
2022-11-18 23:31:05 +0300

    Улучшение отображения в админке

--- msbooks_engine/admin.py
+++ msbooks_engine/admin.py
@@ -4,7 +4,10 @@ from .models import Subscriber
 
 
 class SubscriberAdmin(admin.ModelAdmin):
-    list_display = ('email', 'name', '__str__')
+    list_display = ('pk', 'email', 'name', '__str__')
+    list_display_links = ('pk', 'email', 'name')
+    list_filter = ('email', 'name')
+    search_fields = ('email', 'name')
     ordering = ('email',)
 
 
