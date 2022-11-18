57e167f035bdc8a8ce9cccbb1be4c95e7a63b034
facetable <F201283070480f>
2022-11-18 23:11:34 +0300

    Запись данных из формы в БД

--- msbooks_engine/views.py
+++ msbooks_engine/views.py
@@ -1,11 +1,18 @@
-import datetime
-
 from django.shortcuts import render
+
+from .forms import SubscriberForm
 
 
 def index(request):
-    date = datetime.datetime.now().date()
+    form = SubscriberForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        print(request.POST)
+        print(form.cleaned_data)
+        form.save()
+
     context = {
-        'date': date
+        'form': form
     }
+
     return render(request, 'msbooks_engine/index.html', context)
